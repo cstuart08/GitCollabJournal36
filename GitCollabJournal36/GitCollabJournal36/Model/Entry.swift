7d23a9b88cc25dd0b946e88c89e991a7019ae6f5
Cameron Stuart <cameron@cameronstuart.com>
2020-10-14 12:27:37 -0400

    Adds Entry Model.

---
+++ GitCollabJournal36/GitCollabJournal36/Model/Entry.swift
@@ -0,0 +1,16 @@
+//
+//  Entry.swift
+//  GitCollabJournal36
+//
+//  Created by Cameron Stuart on 10/14/20.
+//
+
+import Foundation
+
+class Entry {
+    var bodyText: String
+    
+    init(bodyText: String) {
+        self.bodyText = bodyText
+    }
+}
